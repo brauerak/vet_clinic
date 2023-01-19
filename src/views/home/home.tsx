9160706e9756b9c29c7039aec057794d4fd8e969
Piotr Brauer <brauer10@gmail.com>
2023-01-19 18:07:47 +0100

    PhotoContainer was added to HomeView

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,5 +1,11 @@
+import { url } from "inspector"
+import { PhotoContainer } from "../../styled/photo-container/photo-container"
+
 const HomeView: React.FC = () => {
-    return <>HomeView</>
+    return <>
+    <PhotoContainer direction="row" height='300px' url="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-732x549.jpg" />
+    
+    </>
 }
 
 export default HomeView
