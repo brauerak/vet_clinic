27e87d4fa0787a593cffc03f7c393b3f7d1c7856
Piotr Brauer <brauer10@gmail.com>
2023-01-20 19:26:57 +0100

    ContentContainer was added to HomeView

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,10 +1,10 @@
 import { url } from "inspector"
+import ContentContainer from "../../styled/content-container/content-container"
 import { PhotoContainer } from "../../styled/photo-container/photo-container"
 
 const HomeView: React.FC = () => {
     return <>
-    <PhotoContainer direction="row" height='300px' url="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-732x549.jpg" />
-    
+    <ContentContainer/>
     </>
 }
 
