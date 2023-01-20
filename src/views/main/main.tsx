ee3d0b2f022d6b9eade3ece48a9b5613cb49db85
Piotr Brauer <brauer10@gmail.com>
2023-01-20 19:24:24 +0100

    photo-container was updated in MainView

--- src/views/main/main.tsx
+++ src/views/main/main.tsx
@@ -3,11 +3,16 @@ import { Outlet } from "react-router-dom";
 import  { Container }  from "../../styled/container/container";
 import ResponsiveAppBar from "../../components/app-bar/app-bar";
 import { Footer } from "../../styled/footer/footer";
+import { PhotoContainer } from "../../styled/photo-container/photo-container";
 
 const MainView: React.FC = () => {
   return <>
     <ResponsiveAppBar/>
     <Container direction="column">
+    <PhotoContainer direction="column" url="https://i.pinimg.com/564x/c6/ad/fc/c6adfcce6f3c25bb55ab0c071188d8fd.jpg">
+       {/* <h2 className="vetClinicLogo">VetClinic</h2> */}
+       <h5 className="vetClinicLogo"> Całodobowa Klinika Weterynaryjna</h5>
+    </PhotoContainer>
     <Outlet />
     </Container>
     <Footer direction = 'row'>
