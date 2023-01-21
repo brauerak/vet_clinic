b8e07034eb8f916f43176d140f683883e9034fb1
Piotr Brauer <brauer10@gmail.com>
2023-01-21 18:04:14 +0100

    PhotoContainer was added to HomeView in home.tsx

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -4,6 +4,10 @@ import { PhotoContainer } from "../../styled/photo-container/photo-container"
 
 const HomeView: React.FC = () => {
     return <>
+        <PhotoContainer direction="column" url="https://i.pinimg.com/564x/c6/ad/fc/c6adfcce6f3c25bb55ab0c071188d8fd.jpg">
+        <h5 className="vetClinicLogo"> Całodobowa Klinika Weterynaryjna</h5>
+    </PhotoContainer>
+
     <ContentContainer/>
     </>
 }
