fbe285acc23ae4d12d8a193e26a03977ddba3ede
Piotr Brauer <brauer10@gmail.com>
2023-01-21 18:04:47 +0100

    PhotoContainer was added to ContactView in contact.tsx

--- src/views/contact/contact.tsx
+++ src/views/contact/contact.tsx
@@ -1,6 +1,11 @@
+import { PhotoContainer } from "../../styled/photo-container/photo-container";
+
 const ContactView: React.FC = () => {
     return <>
-    ContactView
+       <PhotoContainer direction="column" url="https://i.pinimg.com/564x/d7/f8/78/d7f878731a866f54eb28d3fe634d6e6a.jpg">
+        <h5 className="vetClinicLogo"> Całodobowa Klinika Weterynaryjna</h5>
+    </PhotoContainer>
+
     </>
 };
 
