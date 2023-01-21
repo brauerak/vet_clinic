9939f366793439c1b6821d7b65bef7b1b2131f78
Piotr Brauer <brauer10@gmail.com>
2023-01-21 18:05:14 +0100

    PhotoContainer was added to AppointmentsView in appointments.tsx

--- src/views/appointments/appointments.tsx
+++ src/views/appointments/appointments.tsx
@@ -1,5 +1,13 @@
+import Form from "../../components/form/form"
+import { PhotoContainer } from "../../styled/photo-container/photo-container"
+
 const AppointmentsView: React.FC = () => {
-    return <>Control visit view</>
+    return <>
+        <PhotoContainer direction="column" url="https://i.pinimg.com/564x/32/70/b0/3270b0eacbacfb52967406331ea75fa2.jpg">
+        <h5 className="vetClinicLogo"> Całodobowa Klinika Weterynaryjna</h5>
+    </PhotoContainer>
+    <Form/>
+    </>
 }
 
 export default AppointmentsView
