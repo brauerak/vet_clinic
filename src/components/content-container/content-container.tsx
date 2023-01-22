59b0c51a0699bbbb43903fe2993bdcea14779b3c
Piotr Brauer <brauer10@gmail.com>
2023-01-22 17:17:11 +0100

    content and content-container was created

---
+++ src/components/content-container/content-container.tsx
@@ -0,0 +1,12 @@
+import { Content } from "../../styled/content/content"
+
+
+const ContentContainer: React.FC = () => {
+    return<Content>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat non beatae voluptatem debitis modi temporibus neque? Magnam nisi voluptatem obcaecati quo officia? Expedita cupiditate voluptatibus incidunt dignissimos architecto doloremque vitae.
+   </Content>
+    
+}
+
+export default ContentContainer
+
---
+++ src/styled/content/content.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 500px;
+    font-size: 2rem;
+    font-weight: 700;
+`
