4a229f6d6281ac6e1a9a68f3e2005645b2075c3f
Piotr Brauer <brauer10@gmail.com>
2023-01-25 18:57:23 +0100

    ConteinerProps in Container container was deleted. Style in Container container was updated.

--- src/styled/container/container.tsx
+++ src/styled/container/container.tsx
@@ -1,15 +1,9 @@
 import styled from "@emotion/styled";
 
-interface ContainerProps {
-    direction: "column" | "row"
-};
 
-export const Container = styled.div<ContainerProps>`
-    display: flex;
-    flex-flow: ${(props) => `${props.direction}`} nowrap;
-    height: 100vh;
+
+export const Container = styled.div`
     background-color: white;
-    color: black
     
 `;
 
