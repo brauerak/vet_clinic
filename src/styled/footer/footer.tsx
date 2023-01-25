dc253a93cd89dcb5686bc9b77b79f162900575aa
Piotr Brauer <brauer10@gmail.com>
2023-01-25 18:53:27 +0100

    FooterProps was deleted and Footer style was updated

--- src/styled/footer/footer.tsx
+++ src/styled/footer/footer.tsx
@@ -1,13 +1,10 @@
 import styled from "@emotion/styled";
 
-interface FooterProps {
-    direction: "column" | "row"
-};
+;
 
-export const Footer = styled.div<FooterProps>`
-    display: flex;
-    flex-flow: ${(props) => `${props.direction}`} nowrap;
-    height: 100px;
+export const Footer = styled.div`
+    height: 150px;
+    width: 100%;
     background-color: #b2dfdb;
     
 `;
