56e86cb5c0b2ce627abb2b93ae58b1037ed745d0
Piotr Brauer <brauer10@gmail.com>
2023-01-25 18:52:05 +0100

    PhotoContainer was updated.

--- src/styled/photo-container/photo-container.tsx
+++ src/styled/photo-container/photo-container.tsx
@@ -2,26 +2,26 @@ import styled from "@emotion/styled";
 import { Url } from "url";
 
 interface ContainerProps {
-    direction: "column" | "row",
     url: string
 };
 
 export const PhotoContainer = styled.div<ContainerProps>`
-    display: flex;
-    flex-flow: ${(props) => `${props.direction}`};
-    align-items: center;
+    display: grid;
+    align-content: flex-end;
+    /* align-items: center;  */
     justify-content: center;
     color: white;
     font-size: 30px;
     font-weight: bold;
-    height: 700px;
+    height: 91vh;
     margin-right: 0px;
+
     width: 100%;
     background-image: url(${(props) => `${props.url}`});
     background-repeat: no-repeat;
     background-size: 100%;
-    background-position: center;
-    opacity: 0.8;
+    background-position-y: -60px;
+    /* opacity: 0.8; */
     
 
     `
