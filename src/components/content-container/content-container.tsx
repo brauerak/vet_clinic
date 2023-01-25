26696433d41404d1d2ae4ff8e2779f6ebce859f0
Piotr Brauer <brauer10@gmail.com>
2023-01-25 19:00:13 +0100

    Content in ContentContainer was changed.

--- src/components/content-container/content-container.tsx
+++ src/components/content-container/content-container.tsx
@@ -1,12 +1,38 @@
-import { Content } from "../../styled/content/content"
-
+import { Container } from "../../styled/container/container";
+import { ContentText } from "../../styled/content/contentText";
 
 const ContentContainer: React.FC = () => {
-    return<Content>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat non beatae voluptatem debitis modi temporibus neque? Magnam nisi voluptatem obcaecati quo officia? Expedita cupiditate voluptatibus incidunt dignissimos architecto doloremque vitae.
-   </Content>
-    
-}
+  return (
+    <Container>
+      <ContentText fontSize="2rem" fontWeight="300" color="black">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+        praesentium quidem obcaecati inventore in, accusamus veniam, libero a
+        hic sint sunt consectetur beatae tenetur eius deserunt! Ratione id nobis
+        laboriosam.
+      </ContentText>
 
-export default ContentContainer
+      <ContentText fontSize="2rem" fontWeight="300" color="black">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+        praesentium quidem obcaecati inventore in, accusamus veniam, libero a
+        hic sint sunt consectetur beatae tenetur eius deserunt! Ratione id nobis
+        laboriosam.
+      </ContentText>
+      <ContentText fontSize="1.5rem" fontWeight="300" color="black">
+        <ContentText fontSize="1.5rem" fontWeight="300" color="black">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
+          aut dolor repellendus, accusantium optio magnam, numquam cupiditate
+          quos sed expedita obcaecati omnis consequuntur nam. Soluta sed
+          similique praesentium voluptatem deserunt?
+        </ContentText>
+        <ContentText fontSize="1.5rem" fontWeight="300" color="black">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim velit
+          ipsa nisi, pariatur corrupti nobis, tenetur cum optio laborum, fugiat
+          maiores quaerat aliquid! Eveniet, in fugiat. Accusantium omnis
+          asperiores voluptates!
+        </ContentText>
+      </ContentText>
+    </Container>
+  );
+};
 
+export default ContentContainer;
