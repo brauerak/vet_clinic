0398e380d5dfa7e806cba9813b53ba20c51ee246
Piotr Brauer <brauer10@gmail.com>
2023-01-28 09:21:02 +0100

    FormContainer was added to appoint.

--- src/views/appointments/appointments.tsx
+++ src/views/appointments/appointments.tsx
@@ -1,26 +1,28 @@
-import { Typography } from "@mui/material"
-import Form from "../../components/form/form"
-import Logo from "../../components/logo/logo"
-import BootstrapButton from "../../styled/buttons/btn-boostrap"
-import { Content } from "../../styled/content/content"
-import { PhotoContainer } from "../../styled/photo-container/photo-container"
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { Typography } from "@mui/material";
+import Form from "../../components/form/form";
+import Logo from "../../components/logo/logo";
+import BootstrapButton from "../../styled/buttons/btn-boostrap";
+import { Content } from "../../styled/content/content";
+import { PhotoContainer } from "../../styled/photo-container/photo-container";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { Container } from "../../styled/container/container";
+import FormContainer from "../../components/form/form-container";
 
 const AppointmentsView: React.FC = () => {
-    return <>
+  return (
+    <>
         <PhotoContainer url="https://www.worldatlas.com/r/w960-q80/upload/8b/72/3e/shutterstock-690150508.jpg">
-        <BootstrapButton>
+          <BootstrapButton>
             Wypełnij formularz
-            <KeyboardArrowDownIcon fontSize="large"/>
-        
-        </BootstrapButton>
-        <Content marginTop="30px" marginBottom="10px">
-        <Logo />
-        </Content>
-
+            <KeyboardArrowDownIcon fontSize="large" />
+          </BootstrapButton>
+          <Content marginTop="30px" marginBottom="10px">
+            <Logo />
+          </Content>
         </PhotoContainer>
-    <Form/>
+        <FormContainer/>
     </>
-}
+  );
+};
 
-export default AppointmentsView
+export default AppointmentsView;
